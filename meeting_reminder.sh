c792af4aa4188c8233c63d953e45825515996f4d
Ojok Simon Peter <simonpeter.oj@gmail.com>
2023-04-23 01:07:40 +0300

    Fix name error

--- meeting_reminder.sh
+++ meeting_reminder.sh
@@ -7,5 +7,5 @@ meeting_info=$(zenity --forms \
 	--forms-date-format='%Y-%m-%d' \
 	2>/dev/null)
 if [[ -n "$meeting_info" ]]; then
-	python3 send_reminders.py "$meeting_info"
+	python3 meeting_reminder.py "$meeting_info"
 fi
